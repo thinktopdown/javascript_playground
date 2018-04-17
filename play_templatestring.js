2c66b9b33a4efd7ff68549b2cf8fcc85f1064d87
Adam Huda <adam@thinktopdown.com>
2018-04-17 10:02:39 -0700

    fix spelling lint

--- play_templatestring.js
+++ play_templatestring.js
@@ -2,6 +2,6 @@ const x = 1
 const templateString1 = "test ${x}"
 console.log(templateString1)
 
-// use backticks!
+// use back ticks!
 const templateString2 = `test ${x}`
 console.log(templateString2)
